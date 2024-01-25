36ace002e1e0e6d2a04c5cc345676a94e36a0a33
nour kessentini <kesentininour123@gmail.com>
2024-01-25 12:24:31 +0100

    Utilisation des éléments de la BD pour créer elemntLevel page d accueil

--- Src/backend/getNiveaux.php
+++ Src/backend/getNiveaux.php
@@ -29,7 +29,6 @@ function getNiveaux() {
 
     $connexion = $GLOBALS['connexion'];
     
-
     $Niveau =  new stdClass;
     $Niveau->accessible = array();
     $Niveau->bloque = array();
@@ -40,56 +39,44 @@ function getNiveaux() {
         $requete = "SELECT * FROM NIVEAU WHERE id <= (SELECT MAX(id_niveau+1) FROM COMPLETER WHERE id_utilisateur = ". $_SESSION['auth'] ." ) OR id = 1 LIMIT $limiteAffichage;";
         $resultat = mysqli_query($connexion, $requete);
 
-
         while ($ligne = mysqli_fetch_assoc($resultat)){
-            $id = $ligne['id'];
-            $nom = $ligne['nom'];
-            $difficulte = $ligne['difficulte'];
-            $theme = $ligne['id_theme'];
-            $Niveau->accessible[$id] = array();
-            array_push($Niveau->accessible[$id],$nom);
-            array_push($Niveau->accessible[$id],$difficulte);
-            array_push($Niveau->accessible[$id],getTheme($theme));
+            $item = new stdClass();
+            $item->id =  $ligne['id'];
+            $item->family = utf8_encode($ligne['nom']);
+            $item->difficulty =$ligne['difficulte'];
+            $item->theme = getTheme($ligne['id_theme']); 
+            array_push($Niveau->accessible,$item);
         }
-
-        //print '<pre>'; print_r($Niveau->accessible); print '</pre>';
 
         $dernierId = end($Niveau->accessible);
         $nouvelleLimite = $limiteAffichage - count($Niveau->accessible);
 
         $requete = "SELECT * FROM NIVEAU WHERE id > $dernierId LIMIT $nouvelleLimite;";
         $resultat = mysqli_query($connexion, $requete);
-
+ 
         while ($ligne = mysqli_fetch_assoc($resultat)){
-            $id = $ligne['id'];
-            $nom = $ligne['nom'];
-            $difficulte = $ligne['difficulte'];
-            $theme = $ligne['id_theme'];
-            $Niveau->bloque[$id] = array();
-            array_push($Niveau->bloque[$id],$nom);
-            array_push($Niveau->bloque[$id],$difficulte);
-            array_push($Niveau->bloque[$id],getTheme($theme));
+            $item = new stdClass();
+            $item->id =  $ligne['id'];
+            $item->family = utf8_encode($ligne['nom']);
+            $item->difficulty =$ligne['difficulte'];
+            $item->theme = getTheme($ligne['id_theme']); 
+            array_push($Niveau->bloque,$item);
         }
-        
-        //print '<pre>'; print_r($Niveau); print '</pre>';
     }else {
         $requete = "SELECT * FROM NIVEAU LIMIT $limiteAffichage;";
         $resultat = mysqli_query($connexion, $requete);
-
+     
         while ($ligne = mysqli_fetch_assoc($resultat)){
-            $id = $ligne['id'];
-            $nom = utf8_encode($ligne['nom']);
-            $difficulte = $ligne['difficulte'];
-            $theme = $ligne['id_theme'];
-            $Niveau->bloque[$id] = array();
-            array_push($Niveau->bloque[$id],$nom);
-            array_push($Niveau->bloque[$id],$difficulte);
-            array_push($Niveau->bloque[$id],getTheme($theme));
+            $item = new stdClass();
+            $item->id =  $ligne['id'];
+            $item->family = utf8_encode($ligne['nom']);
+            $item->difficulty =$ligne['difficulte'];
+            $item->theme = getTheme($ligne['id_theme']); 
+            array_push($Niveau->bloque,$item);
         }
         
-    }//print '<pre>'; print_r($Niveau); print '</pre>';
+    }
 
-    
     return json_encode($Niveau);
 }
 
--- Src/frontend/js/App.js
+++ Src/frontend/js/App.js
@@ -1,9 +1,35 @@
 const createLevelElement = (level) => {
     let template = document.getElementById("level__model");
     let copy = document.importNode(template.content, true);
-    copy.querySelector("[data-type='level__ref']").setAttribute('href', level.url);
+    copy.querySelector("[data-type='level__ref']").setAttribute('href', level.id);
     copy.querySelector("[data-type='level__family']").textContent=level.family;
     copy.querySelector("[data-type='level__theme']").textContent=level.theme;
+    for (let i = 0; i < level.difficulty; i++) {
+        let star = document.createElement("i");
+        star.setAttribute("class", "fa-solid fa-star");
+        copy.querySelector("[data-type='level__difficulty']").appendChild(star);
+    }
     document.getElementById("main").appendChild(copy);
 }
 
+/*
+fetch("./backend/getNiveaux.php?lim=9", {method : 'get'})
+.then(res => res.json())
+.then(data => {
+    for (let i = 0; i < data.accessible.length; i++) {
+       createLevelElement(data.accessible[i])
+    }
+    for (let i = 0; i < data.bloque.length; i++) {
+        createLevelElement(data.bloque[i])
+    }
+})
+.catch(err => console.log(err))
+*/
+
+let test = {
+    id: 12,
+    family: 12,
+    theme: 'boucle',
+    difficulty: 3
+}
+createLevelElement(test);
