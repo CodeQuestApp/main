73e2acc5d8ad1b56c18d81bd210cf60abe6e1667
nour kessentini <kesentininour123@gmail.com>
2024-03-05 11:53:41 +0100

    ajout des regles d'ergonomie

--- Src/frontend/js/App.js
+++ Src/frontend/js/App.js
@@ -1,8 +1,8 @@
 const createLevelElement = (level) => {
     let template = document.getElementById("item__model");
     let copy = document.importNode(template.content, true);
-    copy.querySelector("[data-type='level__ref']").setAttribute('id', "./level_algo.html?id=" + level.id);
-    copy.querySelector("[data-type='level__ref']").setAttribute('href', "level" + level.id);
+    copy.querySelector("[data-type='level__ref']").setAttribute('href', "./level_algo.html?id=" + level.id);
+    copy.querySelector("[data-type='level__ref']").setAttribute('id', "level" + level.id);
     copy.querySelector("[data-type='level__family']").textContent=level.family;
     copy.querySelector("[data-type='level__theme']").textContent=level.theme;
     for (let i = 0; i < level.difficulty; i++) {
@@ -26,8 +26,8 @@ fetch("./backend/getNiveaux.php?lim=9", { method: 'get' })
             createLevelElement(data.accessible[i])
         }
         for (let i = 0; i < data.bloque.length; i++) {
-            createLevelElement(data.bloque[i])
-            document.getElementById("level"+data.bloque[i]).classList.add("grey-level")
+            createLevelElement(data.bloque[i]);
+            document.getElementById("level"+data.bloque[i].id).classList.add("inaccessible");
         }
     })
     .catch(err => console.log(err))
