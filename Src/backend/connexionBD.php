5a1d200de7206226454bfe163539abee61442094
Alex Moreno <alex.moreno32390@gmail.com>
2024-03-12 17:23:38 +0100

    Correction de la connexion

--- Src/backend/connexionBD.php
+++ Src/backend/connexionBD.php
@@ -1,9 +1,9 @@
 <?php
 
-    $user = "351424_bd";                          //
-    $pass = "codequestapp_bd";                      //  Infos pour la connexion 
-    $bdd = "codequestapp_bd";                     //  à la BD
-    $host = "mysql-codequestapp.alwaysdata.net"; // Pour alwaysdata
+    $user = "adumolie_bd";                    
+    $pass = "adumolie_bd";                    
+    $bdd = "adumolie_bd";                 
+    $host = "lakartxela.iutbayonne.univ-pau.fr"; 
         
     $connexion = mysqli_connect($host, $user, $pass, $bdd); // connexion à la BD
 
